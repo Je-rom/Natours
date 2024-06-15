2245e7a705931d3270e2c5618f13e9fa33e07320
Jerry Abadi <abadijerry44@gmail.com>
2024-06-15 00:59:21 +0000

    database connection

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const app = require('./app');
+
+dotenv.config({path: './.env'});
+
+const DB = process.env.CONNECTION_STRING;
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('DB connection successful!'))
+.catch((err) => console.error('DB connection error:', err));
 
 app.listen(7000, () => {
   console.log('listening on port 7000');
