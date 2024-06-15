7c28df54f5ee84da29e3e8f6574b8de2de39a7b5
Jerry Abadi <abadijerry44@gmail.com>
2024-06-15 22:34:06 +0000

    Models

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = require('./app');
 
@@ -12,36 +11,6 @@ mongoose.connect(DB, {
 })
 .then(() => console.log('DB connection successful!'))
 .catch((err) => console.error('DB connection error:', err));
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "a tour must have a name"],
-    unique: true,
-  },
-  price: {
-    type: Number,
-    default: 4.5,
-  },
-  rating: {
-    type: Number,
-    required: [true, "a tour must have a price"]
-  }
-});
-
-const Tour = mongoose.model("Tour", tourSchema)
-const tourDb = new Tour({
-  name: "englnd",
-  price: 699,
-  rating: 4.0
-});
-tourDb.save().then(doc=>{
-  console.log(doc, 'saved')
-}).catch(err=>{
-  console.log(err,'couldnt save')
-});
-
-
 
 app.listen(7000, () => {
   console.log('listening on port 7000');
