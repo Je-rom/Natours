0e1ebc1a7654f325cbf7e8691f76f3c72d6b0508
Jerry Abadi <abadijerry44@gmail.com>
2024-06-18 16:50:08 +0000

    data unwinding

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.route('/top-5-cheap').get(tourController.getCheapTours ,tourController.getAllTours)
 router.route('/tour-stats').get(tourController.getTourStats)
+router.route('/monthly-plan/:year').get(tourController.getMontlyPlan)
 router
   .route('/')
   .get(tourController.getAllTours)
