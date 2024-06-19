8efb7cc9ff3b36ea022381c375a873f1602207da
Jerry Abadi <abadijerry44@gmail.com>
2024-06-19 00:16:24 +0000

    data validation

--- models/tourModels.js
+++ models/tourModels.js
@@ -5,7 +5,9 @@ const tourSchema = new mongoose.Schema({
       type: String,
       required: [true, "a tour must have a name"],
       unique: true,
-      trim:true
+      trim:true,
+      maxLength: [40, 'a tour should have less or 40 characters'],
+      minlength: [5, 'a tour should have more than 5 characters']
     },
     duration: {
       type: Number,
@@ -18,10 +20,16 @@ const tourSchema = new mongoose.Schema({
     difficulty: {
       type: String,
       required: [true, "a tour must have a difficulty"],
+      enum: {
+        values: ['easy', 'meduim', 'difficult'],
+        message: 'level has to be easy, meduim or difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'ratings cannot be less than 1.0'],
+      max: [5, 'ratings cannot be more than 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -31,7 +39,15 @@ const tourSchema = new mongoose.Schema({
       type: Number,
       required: [true, "a tour must have a price"]
     },
-    priceDiscount:Number,
+    priceDiscount:{
+      type: Number,
+      validate: {
+        validator: function(val){
+          return val < this.price //price discount should be less than the price, the this keyword wont work on update tour api only to create
+        },
+        message: '{VALUE} should not be more than the price amount'
+      }
+      },
     summary:{
       type: String,
       trim: true,
@@ -63,6 +79,6 @@ const tourSchema = new mongoose.Schema({
   tourSchema.pre('find', function(next){
     this.find({secretTour: {$ne: true}})
     next();
-  })
+  });
 
   module.exports = Tour;
