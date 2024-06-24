b3b530caf2a94f26c2363be0ce8458e360d5f096
Jerry Abadi <abadijerry44@gmail.com>
2024-06-24 00:24:28 +0000

    data sanitization

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const globalErrorHandler = require('./controller/errorController')
 const AppError = require ('./utils/appError');
-const rateLimit = require('express-rate-limit')
+const rateLimit = require('express-rate-limit');
+const helmet = require ('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 
 
 const app = express();
 app.use(express.static("public"))
 
+//security http headers
+app.use(helmet());
+
+// rate limit
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -18,11 +25,14 @@ const limiter = rateLimit({
 
 app.use('/api', limiter)
 
-
 //middlewares
 app.use(morgan('dev'));
 
 app.use(express.json()); //parses JSON request bodies and attaches the parsed data to req.body.
+//data sanitization against noSql query injection
+app.use(mongoSanitize());
+//sanitize against xss- clean user input from html code
+app.use(xss())
 
 //for logging, authentication checks, setting headers, or any other custom logic, it applies to all routes/request
 app.use((req, res, next) => {
